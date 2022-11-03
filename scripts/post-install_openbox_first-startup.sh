eedea26381f865af70d4c3a4507672bec1fb5d36
Daniel Sacré <daniel.sacre@gmx.de>
2022-11-03 18:52:40 +0100

    modified first startup procedure

--- scripts/post-install_openbox_first-startup.sh
+++ scripts/post-install_openbox_first-startup.sh
@@ -1,10 +1,10 @@
 # Set the wallpaper and save the settings
-nitrogen --save --set-zoom-fill ~/.config/wallpaper/oskar_ascii_orange-white_1920x1080.png 
+nitrogen --save --set-zoom-fill ~/.config/wallpaper/oskar_ascii_orange-white_1920x1080.png &
 
 # Setting the keymap, so it is persistant between sessions
 # WARNING: Currently hardcoded to de layout, since setting it with the original ArchTitus procedure did not work
-localectl --no-ask-password set-x11-keymap de
+localectl --no-ask-password set-x11-keymap de & # not working
 
 # Delete autostart file for first start and rename backup file
-rm -f autostart
-mv autostart.bak autostart
+# not working
+rm -f autostart && mv autostart.bak autostart &
