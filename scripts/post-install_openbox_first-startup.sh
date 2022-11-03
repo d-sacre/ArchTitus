1693f682d4c87db32289c9805bd606c7bf050c8a
Daniel Sacré <daniel.sacre@gmx.de>
2022-11-03 18:35:35 +0100

    added experimental openbox setup during first autostart

---
+++ scripts/post-install_openbox_first-startup.sh
@@ -0,0 +1,10 @@
+# Set the wallpaper and save the settings
+nitrogen --save --set-zoom-fill ~/.config/wallpaper/oskar_ascii_orange-white_1920x1080.png 
+
+# Setting the keymap, so it is persistant between sessions
+# WARNING: Currently hardcoded to de layout, since setting it with the original ArchTitus procedure did not work
+localectl --no-ask-password set-x11-keymap de
+
+# Delete autostart file for first start and rename backup file
+rm -f autostart
+mv autostart.bak autostart
